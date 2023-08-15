831d4e09e538ca077b9e40a97bd08e0575785759
MannDev10 <fm200467@gmail.com>
2023-08-15 15:32:23 +0700

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require(`dotenv`).config();
 const express = require("express");
 const app = express();
 const db = require("./src/helpers/database");
-const port = process.env.PORT_WEB;
+const port = 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const serverless = require("serverless-http");
